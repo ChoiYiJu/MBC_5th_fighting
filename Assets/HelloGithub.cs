6469cdd106c5791bfd142857dc09e8fd15728847
ChoiYiJu <yijuju0621@gmail.com>
2025-10-22 12:53:31 +0900

    코드 내용 수정

--- Assets/HelloGithub.cs
+++ Assets/HelloGithub.cs
@@ -7,7 +7,7 @@ public class HelloGithub : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("헬로우 월드다");
     }
 
     // Update is called once per frame
